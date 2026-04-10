ff23e0d3a1e0be5caa3a41df8560501b8f8511f2
Meu Nome <joelma.carmo.duarte@gmail.com>
2026-04-10 16:07:43 -0300

    Exercício Piada Concluído

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("---- O LIVRO ----");
+Console.WriteLine();
+Console.WriteLine("O que fala o livro de Matemática para o livro de História?");
+Console.WriteLine("- Não me venha com história que eu já estou cheio de problema!");
+Console.WriteLine();
